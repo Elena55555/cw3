5eef8d3a7a7bcf4a87e4ce764ea1ae4d78eecff5
Elena <elenela@yandex.ru>
2023-06-07 16:57:00 +0300

    ДЗ 3!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("дз 3");
+        System.out.println("Домашняя работа № 3");
+
+
+        Car[] cars = {
+                new Car("car1", 4),
+                new Car("car2", 4),
+
+        };
+
+        Bicycle[] bicycles = {
+                new Bicycle("bicycle1", 2),
+                new Bicycle("bicycle2", 2),
+        };
+        Truck[] trucks = {
+                new Truck("truck1", 6),
+                new Truck("truck2", 8),
+        };
+
+        Transport[] transports = new Transport[cars.length + bicycles.length + trucks.length];
+
+        for (int i = 0; i < cars.length; i++) {
+            transports[i] = cars[i];
+        }
+        for (int i = 0; i < bicycles.length; i++) {
+            transports[i + cars.length] = bicycles[i];
+        }
+        for (int i = 0; i < trucks.length; i++) {
+            transports[i + cars.length + bicycles.length] = trucks[i];
+        }
+
+
+        for (Transport transport : transports) {
+            transport.checkTrailer();
+            transport.checkEngine();
+            transport.updateTyre();
+
+        }
+
+
+
+
     }
 }
+
